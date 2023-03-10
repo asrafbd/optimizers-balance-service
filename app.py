ff1c8c5465a955f31390e683d60d28a99f42ef13
amdadulbari <amdadulbari@gmail.com>
2023-03-10 14:47:16 +0600

    Project initialized

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,jsonify,request
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+# set up connection to PostgreSQL database
+database_name = os.getenv('DATABASE_NAME')
+database_user = os.getenv('DATABASE_USER')
+database_password = os.getenv('DATABASE_PASSWORD')
+database_host = os.getenv('DATABASE_HOST')
+database_port = os.getenv('DATABASE_PORT')
+
+conn = psycopg2.connect(
+    database=database_name,
+    user=database_user,
+    password=database_password,
+    host=database_host,
+    port=database_port
+)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route('/summation',methods = ['POST'])
+def summation():
+    num = request.json['num']
+    cur = conn.cursor()
+    cur.execute("select sum(balance) from balance where num=%s", (num,))
+    row = cur.fetchone()
+    if row is None:
+        return jsonify({'sum': 'User not found'})
+    else:
+        return jsonify({'sum': row[0]})
+
+if __name__ == "__main__":
+     app.run()
